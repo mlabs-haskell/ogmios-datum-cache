3013dc9106d5ee4b825afd6b6600ee0efa7cd0e2
Kirill Boltaev <aske@fmap.me>
2022-01-10 16:05:10 +0300

    wip, get multiple datums

--- src/Api/Handler.hs
+++ src/Api/Handler.hs
@@ -10,10 +10,11 @@ import qualified Data.ByteString.Lazy as BSL
 import Codec.Serialise (Serialise, deserialiseOrFail)
 import Data.Function ((&))
 import Control.Monad.IO.Class (liftIO)
+import qualified Data.Vector as Vector
 
 import qualified PlutusData
 import Api
-import Database (getDatumSession, Datum (value))
+import Database (getDatumSession, Datum (value), getDatumsSession)
 
 datumServiceHandlers :: Hasql.Connection -> Routes (AsServerT Handler)
 datumServiceHandlers pgConn = Routes{..}
@@ -21,13 +22,19 @@ datumServiceHandlers pgConn = Routes{..}
     datumRoutes :: ToServant DatumApi (AsServerT Handler)
     datumRoutes = genericServerT DatumApi{..}
 
+    toPlutusData datumRes =
+      deserialiseOrFail @PlutusData.Data (BSL.fromStrict $ value datumRes) & either (const $ throwError err500) pure
+
     getDatumByHash :: Text -> Handler GetDatumByHashResponse
     getDatumByHash hash = do
       datumRes <- liftIO (Session.run (getDatumSession hash) pgConn) >>= either (const $ throwError err404) pure
-      plutusData <-
-        deserialiseOrFail @PlutusData.Data (BSL.fromStrict $ value datumRes) & either (const $ throwError err500) pure
+      plutusData <- toPlutusData datumRes
+
       pure $ GetDatumByHashResponse plutusData
 
     getDatumsByHashes :: Text -> Handler GetDatumsByHashesResponse
-    getDatumsByHashes hashes = do
-      pure $ GetDatumsByHashesResponse [PlutusData.I 100, PlutusData.I 200]
+    getDatumsByHashes _hashes = do
+      let hashes = ["923918e403bf43c34b4ef6b48eb2ee04babed17320d8d1b9ff9ad086e86f44ec"]
+      datums <- liftIO (Session.run (getDatumsSession hashes) pgConn) >>= either (const $ throwError err404) pure
+      plutusDatums <- Vector.mapM toPlutusData datums
+      pure $ GetDatumsByHashesResponse plutusDatums
