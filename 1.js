368d44cabbb0a73bbb4950268f7500d7ce7f23b4
Kirill Boltaev <aske@fmap.me>
2022-01-31 16:50:14 +0300

    Add more examples to a sample js client

--- 1.js
+++ 1.js
@@ -4,6 +4,7 @@ const client = new WebSocket('ws://localhost:9999/ws');
 client.on('message', function(msg) {
     const response = JSON.parse(msg);
     console.log(response);
+    console.log(JSON.stringify(response));
 });
 
 client.once('open', () => {
@@ -13,8 +14,16 @@ client.once('open', () => {
         version: "1.0",
         servicename: "ogmios",
         methodname: "GetDatumsByHashes",
-        args: { hashes: ["abc"] }
+        args: { hashes: ["abc", "04caaf1336b754e0b8b4e2fa1c59aa6b85f97dd29652729f1c1e28805acdeb20"] }
     });
+    // const req = JSON.stringify({
+    //     type: "jsonwsp/request",
+    //     version: "1.0",
+    //     servicename: "ogmios",
+    //     methodname: "GetDatumByHash",
+    //     // args: { hash: "abc" }
+    //     args: { hash: "04caaf1336b754e0b8b4e2fa1c59aa6b85f97dd29652729f1c1e28805acdeb20" }
+    // });
     console.log(req)
     client.send(req);
 });
