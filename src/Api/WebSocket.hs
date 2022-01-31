fc0eee5a0e4a188bf9cc4127bf6b5bc3f6e5d39e
Kirill Boltaev <aske@fmap.me>
2022-01-31 14:55:35 +0300

    wip

--- src/Api/WebSocket.hs
+++ src/Api/WebSocket.hs
@@ -10,8 +10,9 @@ import Control.Monad.Reader (ask)
 
 import qualified Hasql.Session as Session
 import qualified Data.ByteString.Lazy as BSL
-import Codec.Serialise (deserialiseOrFail)
+import qualified Codec.Serialise as Cbor
 import Data.Either (isLeft)
+import qualified Data.Vector as Vector
 
 import App
 import App.Env
@@ -20,9 +21,54 @@ import Api.WebSocket.Types
 import qualified Database as Db
 import qualified PlutusData
 
+toPlutusData :: Db.Datum -> Either Cbor.DeserialiseFailure PlutusData.Data
+toPlutusData dt = Cbor.deserialiseOrFail @PlutusData.Data (BSL.fromStrict $ Db.value dt)
+
+getDatumByHash :: WS.Connection -> Text -> App ()
+getDatumByHash conn hash = do
+  Env{..} <- ask
+  datumRes <- liftIO (Session.run (Db.getDatumSession hash) envDbConnection)
+  case datumRes of
+    Left _ -> do
+      -- TODO: different response?
+      let resp = mkGetDatumByHashResponse Nothing
+      sendTextData $ Json.encode resp
+    Right datum ->
+      case toPlutusData datum of
+        Left _ -> do
+          let resp = mkGetDatumByHashFault "Error deserializing plutus Data"
+          sendTextData $ Json.encode resp
+        Right plutusData -> do
+          let plutusDataJson = Json.toJSON plutusData
+          let resp = mkGetDatumByHashResponse (Just plutusDataJson)
+          sendTextData $ Json.encode resp
+ where
+   sendTextData = liftIO . WS.sendTextData conn
+
+getDatumsByHashes :: WS.Connection -> [Text] -> App ()
+getDatumsByHashes conn hashes = do
+  Env{..} <- ask
+  datumsRes <- liftIO (Session.run (Db.getDatumsSession hashes) envDbConnection)
+  case datumsRes of
+    Left _ -> do
+      -- TODO: different response?
+      let resp = mkGetDatumsByHashesResponse Nothing
+      sendTextData $ Json.encode resp
+    Right datums -> do
+      case Vector.partition isLeft $ Vector.map (\dt -> case toPlutusData dt of
+                                      Left _ -> Left $ Db.hash dt
+                                      Right plutusData -> Right $ GetDatumsByHashesDatum (Db.hash dt) plutusData) datums of
+        (invalidDatums, validDatums) | Vector.null invalidDatums -> do
+          pure ()
+        (invalidDatums, _) -> do
+          pure ()
+ where
+   sendTextData = liftIO . WS.sendTextData conn
+
+
+
 websocketServer :: WS.Connection -> App ()
 websocketServer conn = forever $ do
-  Env{..} <- ask
   jsonMsg <- receiveData
   case Json.decode @Method jsonMsg of
     Nothing -> do
@@ -30,40 +76,7 @@ websocketServer conn = forever $ do
 
     Just action ->
       case action of
-        GetDatumByHash hash -> do
-          datumRes <- liftIO (Session.run (Db.getDatumSession hash) envDbConnection)
-          case datumRes of
-            Left _ -> do
-              -- TODO: different response?
-              let resp = mkGetDatumByHashResponse Nothing
-              sendTextData $ Json.encode resp
-            Right datum ->
-              case toPlutusData datum of
-                Left _ -> do
-                  let resp = mkGetDatumByHashFault "Error deserializing plutus Data"
-                  sendTextData $ Json.encode resp
-                Right plutusData -> do
-                  let plutusDataJson = Json.toJSON plutusData
-                  let resp = mkGetDatumByHashResponse (Just plutusDataJson)
-                  sendTextData $ Json.encode resp
-        GetDatumsByHashes hashes -> do
-          datumsRes <- liftIO (Session.run (Db.getDatumsSession hashes) envDbConnection)
-          case datumsRes of
-            Left _ -> do
-              -- TODO: different response?
-              let resp = mkGetDatumsByHashes Nothing
-              sendTextData $ Json.encode resp
-            Right datums -> do
-              case partition isLeft $ map (\dt -> case toPlutusData dt of
-                                              Left _ -> Left $ Db.hash dt
-                                              Right plutusData -> Right $ GetDatumsByHashesDatum (Db.hash dt) plutusData) datums of
-                ([], validDatums) -> do
-                  pure ()
-                (invalidDatums, _) -> do
-                  pure ()
-
-
+        GetDatumByHash hash -> getDatumByHash conn hash
+        GetDatumsByHashes hashes -> getDatumsByHashes conn hashes
   where
     receiveData = liftIO $ WS.receiveData conn
-    sendTextData = liftIO . WS.sendTextData conn
-    toPlutusData dt = deserialiseOrFail @PlutusData.Data (BSL.fromStrict $ Db.value dt)
