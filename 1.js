afb6233805474c3f81d3bca24e360954d33cf820
Kirill Boltaev <aske@fmap.me>
2022-02-01 18:55:08 +0300

    More examples for the sample js client

--- 1.js
+++ 1.js
@@ -9,21 +9,34 @@ client.on('message', function(msg) {
 
 client.once('open', () => {
     console.log('opened');
-    const req = JSON.stringify({
-        type: "jsonwsp/request",
-        version: "1.0",
-        servicename: "ogmios",
-        methodname: "GetDatumsByHashes",
-        args: { hashes: ["abc", "04caaf1336b754e0b8b4e2fa1c59aa6b85f97dd29652729f1c1e28805acdeb20"] }
-    });
     // const req = JSON.stringify({
     //     type: "jsonwsp/request",
     //     version: "1.0",
-    //     servicename: "ogmios",
+    //     servicename: "ogmios-datum-cache",
+    //     methodname: "GetDatumsByHashes",
+    //     args: { hashes: ["abc", "04caaf1336b754e0b8b4e2fa1c59aa6b85f97dd29652729f1c1e28805acdeb20"] }
+    // });
+    // const req = JSON.stringify({
+    //     type: "jsonwsp/request",
+    //     version: "1.0",
+    //     servicename: "ogmios-datum-cache",
     //     methodname: "GetDatumByHash",
     //     // args: { hash: "abc" }
     //     args: { hash: "04caaf1336b754e0b8b4e2fa1c59aa6b85f97dd29652729f1c1e28805acdeb20" }
     // });
+    // const req = JSON.stringify({
+    //     type: "jsonwsp/request",
+    //     version: "1.0",
+    //     servicename: "ogmios-datum-cache",
+    //     methodname: "StartFetchBlocks",
+    //     args: { slot: 1, id: "abc" }
+    // });
+    const req = JSON.stringify({
+        type: "jsonwsp/request",
+        version: "1.0",
+        servicename: "ogmios-datum-cache",
+        methodname: "CancelFetchBlocks",
+    });
     console.log(req)
     client.send(req);
 });
