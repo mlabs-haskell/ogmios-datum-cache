b95bac273442f854f1bb7b01d02bb5c28c21488b
t4ccer <t4ccer@gmail.com>
2022-05-04 09:44:43 -0600

    Fix config parser

--- src/Config.hs
+++ src/Config.hs
@@ -2,7 +2,6 @@
 
 module Config (loadConfig, Config (..), BlockFetcherConfig (..)) where
 
-import Control.Applicative ((<|>))
 import Control.Monad.IO.Class (MonadIO)
 import Data.ByteString (ByteString)
 import Data.ByteString.Lazy qualified as LBS
@@ -42,55 +41,28 @@ matchTrue value = Toml.mkMatchError Toml.TBool value
 true :: Toml.Key -> TomlCodec Bool
 true = Toml.match $ Toml.mkAnyValueBiMap matchTrue Toml.Bool
 
-matchFalse :: Toml.Value t -> Either Toml.MatchError Bool
-matchFalse (Toml.Bool False) = Right False
-matchFalse value = Toml.mkMatchError Toml.TBool value
-
-false :: Toml.Key -> TomlCodec Bool
-false = Toml.match $ Toml.mkAnyValueBiMap matchFalse Toml.Bool
-
--- latestBlockT :: Toml.Codec Config FirstBlockInfo
--- latestBlockT = do
---   withDefault False (true "blockFetcher.startFromLast") .= const True
---   pure LatestBlock
-
--- concreateBlockT :: Toml.Codec Config FirstBlockInfo
--- concreateBlockT = do
---   let getConcrete (ConcreateBlock block) = Just block
---       getConcrete _ = Nothing
---   withDefault False (false "blockFetcher.startFromLast") .= const False
---   blockSlot' <- int64 "firstFetchBlock.slot"
---     .= (maybe 0 (maybe 0 blockSlot . getConcrete . cfgFetcherBlock) . cfgFetcher)
---   blockId' <- Toml.text "firstFetchBlock.id"
---     .= (maybe "" (maybe "" blockId . getConcrete . cfgFetcherBlock) . cfgFetcher)
---   pure $ ConcreateBlock $ BlockInfo blockSlot' blockId'
-
-noFetcherT :: Toml.Codec Config (Maybe BlockFetcherConfig)
-noFetcherT = do
-    withDefault False (false "blockFetcher.autoStart") .= const False
-    pure Nothing
-
-blockInfoT :: Toml.Codec Config BlockInfo
+blockInfoT :: TomlCodec BlockInfo
 blockInfoT = do
     blockSlot' <-
-        int64 "firstFetchBlock.slot"
-            .= maybe 0 (blockSlot . cfgFetcherBlock) . cfgFetcher
+        int64 "blockFetcher.firstBlock.slot"
+            .= blockSlot
     blockId' <-
-        Toml.text "firstFetchBlock.id"
-            .= maybe "" (blockId . cfgFetcherBlock) . cfgFetcher
+        Toml.text "blockFetcher.firstBlock.id"
+            .= blockId
     pure $ BlockInfo blockSlot' blockId'
 
-withFetcherT :: Toml.Codec Config (Maybe BlockFetcherConfig)
+withFetcherT :: TomlCodec BlockFetcherConfig
 withFetcherT = do
-    withDefault False (true "blockFetcher.autoStart") .= const True
+    true "blockFetcher.autoStart" .= const True
+    -- bool b Nothing Nothing
     cfgFetcherFilterJson <-
-        withDefault "{ \"const\" = true  }" (Toml.lazyByteString "")
-            .= (maybe "" cfgFetcherFilterJson . cfgFetcher)
-    cfgFetcherBlock <- blockInfoT
+        withDefault "{ \"const\" = true  }" (Toml.lazyByteString "blockFetcher.filter")
+            .= cfgFetcherFilterJson
+    cfgFetcherBlock <- blockInfoT .= cfgFetcherBlock
     cfgFetcherUseLatest <-
-        withDefault False (Toml.bool "")
-            .= maybe False cfgFetcherUseLatest . cfgFetcher
-    pure $ Just BlockFetcherConfig{..}
+        withDefault False (Toml.bool "blockFetcher.startFromLast")
+            .= cfgFetcherUseLatest
+    pure BlockFetcherConfig{..}
 
 configT :: TomlCodec Config
 configT = do
@@ -98,8 +70,7 @@ configT = do
     cfgServerPort <- Toml.int "server.port" .= cfgServerPort
     cfgOgmiosAddress <- Toml.string "ogmios.address" .= cfgOgmiosAddress
     cfgOgmiosPort <- Toml.int "ogmios.port" .= cfgOgmiosPort
-    cfgFetcher <- noFetcherT <|> withFetcherT
-
+    cfgFetcher <- Toml.dioptional withFetcherT .= cfgFetcher
     pure Config{..}
 
 loadConfig :: MonadIO m => m Config
