c7856a703668d17280a6acb763450f2daaf61de7
t4ccer <t4ccer@gmail.com>
2022-06-06 20:12:09 -0600

    Add docs

--- src/Block/Fetch.hs
+++ src/Block/Fetch.hs
@@ -44,7 +44,7 @@ import Data.Maybe (isJust)
 import Data.Text qualified as Text
 import Data.Text.Encoding qualified as Text
 import Hasql.Connection qualified as Hasql
-import Network.Socket qualified as S
+import Network.Socket qualified as Socket
 import Network.WebSockets qualified as WebSockets
 import Network.WebSockets.Stream qualified as Stream
 
@@ -72,17 +72,25 @@ data OgmiosInfo = OgmiosInfo
   }
 
 data BlockFetcherEnv = BlockFetcherEnv
-  { queue :: TBQueue AlonzoBlock
-  , wsConnMVar :: MVar WebSockets.Connection
-  , intersectionTVar :: TVar (Maybe CursorPoint)
+  { -- | Queue used to push blocks for block processor.
+    queue :: TBQueue AlonzoBlock
+  , -- | WS connection to ogmios. Should be used only via 'sendAndReceive'.
+    wsConnMVar :: MVar WebSockets.Connection
+  , -- | Intersection from which block fetcher will start fetching blocks.
+    -- Should be initialized only via `findIntersection`.
+    intersectionTVar :: TVar (Maybe CursorPoint)
   }
 
 data BlockProcessorEnv = BlockProcessorEnv
-  { queue :: TBQueue AlonzoBlock
-  , datumFilterMVar :: MVar DatumFilter
-  , dbConn :: Hasql.Connection
+  { -- | Queue to read blocks from.
+    queue :: TBQueue AlonzoBlock
+  , -- | Datum filer.
+    datumFilterMVar :: MVar DatumFilter
+  , -- | Connection to postgres database.
+    dbConn :: Hasql.Connection
   }
 
+-- | Create new env for block processor.
 mkBlockProcessorEnv ::
   MonadIO m =>
   DatumFilter ->
@@ -94,13 +102,18 @@ mkBlockProcessorEnv f c = do
   datumFilterMVar <- liftIO $ newMVar f
   pure $ BlockProcessorEnv q datumFilterMVar c
 
-mkBlockFetcherEnv :: MonadIO m => BlockProcessorEnv -> WebSockets.Connection -> m BlockFetcherEnv
+-- | Create env for block fetcher connected to block processor's queue
+mkBlockFetcherEnv ::
+  MonadIO m =>
+  BlockProcessorEnv ->
+  WebSockets.Connection ->
+  m BlockFetcherEnv
 mkBlockFetcherEnv processorEnv wsConn = do
   wsConnMVar <- liftIO $ newMVar wsConn
   intersectionTVar <- liftIO $ newTVarIO Nothing
   pure $ BlockFetcherEnv processorEnv.queue wsConnMVar intersectionTVar
 
--- | like 'WebSockets.runClientWith' but without closing socket
+-- | like 'WebSockets.runClientWith' but without closing socket.
 runClientWith' ::
   -- | Host
   String ->
@@ -117,18 +130,19 @@ runClientWith' ::
   IO a
 runClientWith' host port path0 opts customHeaders app = do
   let hints =
-        S.defaultHints
-          { S.addrSocketType = S.Stream
+        Socket.defaultHints
+          { Socket.addrSocketType = Socket.Stream
           }
       fullHost = if port == 80 then host else host ++ ":" ++ show port
       path = if null path0 then "/" else path0
-  addr : _ <- S.getAddrInfo (Just hints) (Just host) (Just $ show port)
-  sock <- S.socket (S.addrFamily addr) S.Stream S.defaultProtocol
-  S.setSocketOption sock S.NoDelay 1
-  S.connect sock (S.addrAddress addr)
+  addr : _ <- Socket.getAddrInfo (Just hints) (Just host) (Just $ show port)
+  sock <- Socket.socket (Socket.addrFamily addr) Socket.Stream Socket.defaultProtocol
+  Socket.setSocketOption sock Socket.NoDelay 1
+  Socket.connect sock (Socket.addrAddress addr)
   stream <- Stream.makeSocketStream sock
   WebSockets.runClientWithStream stream fullHost path opts customHeaders app
 
+-- | Start pair for workers fetching/processing blocks.
 startBlockFetcherAndProcessor ::
   MonadIO m =>
   OgmiosInfo ->
@@ -174,12 +188,17 @@ newtype BlockFetcherApp a = BlockFetcherApp
   }
   deriving newtype (Functor, Applicative, Monad, MonadIO, MonadLogger, MonadReader BlockFetcherEnv)
 
-fetchLoop :: BlockInfo -> BlockFetcherApp ()
+-- | Indefinite loop fetching blocks from ogmios and pushing them to the queue.
+fetchLoop ::
+  (MonadIO m, MonadReader BlockFetcherEnv m, MonadLogger m) =>
+  BlockInfo ->
+  m ()
 fetchLoop blockInfo = forever $ do
   logInfoNS "fetchLoop" "Starting..."
   void $ findIntersection blockInfo
-  forever receiveBlock
+  forever fetchNextBlock
 
+-- | Function to interact with ogmios WS. Use *only* this function to use websocket.
 sendAndReceive ::
   (MonadIO m, MonadReader BlockFetcherEnv m) =>
   ByteString ->
@@ -191,11 +210,9 @@ sendAndReceive toSend = do
       WebSockets.sendTextData wsConn toSend
       WebSockets.receiveData wsConn
 
+-- | Wrapper for 'findIntersection' to be called from user-facing API.
 changeStartingBlock ::
-  ( MonadIO m
-  , MonadReader r m
-  , Has (MVar BlockFetcherEnv) r
-  ) =>
+  (MonadIO m, MonadReader r m, Has (MVar BlockFetcherEnv) r) =>
   BlockInfo ->
   m (Maybe CursorPoint)
 changeStartingBlock blockInfo = do
@@ -204,6 +221,10 @@ changeStartingBlock blockInfo = do
     withMVar env' $ \env ->
       flip runReaderT env . runNoLoggingT . findIntersection $ blockInfo
 
+{- | Find intersection, unblock block fetcher to start fetching block starting from
+ this intersection. If intersection is not found, but was initialized before block
+ fetcher will continue running as before.
+-}
 findIntersection ::
   (MonadIO m, MonadLogger m, MonadReader BlockFetcherEnv m) =>
   BlockInfo ->
@@ -231,32 +252,36 @@ findIntersection blockInfo = do
       liftIO . atomically $ writeTVar env.intersectionTVar $ Just point
       pure $ Just point
 
-receiveBlock :: BlockFetcherApp ()
-receiveBlock = do
+{- | Request and get block from ogmios and push it to queue. Will block until
+ intersection is set.
+-}
+fetchNextBlock ::
+  (MonadIO m, MonadLogger m, MonadReader BlockFetcherEnv m) =>
+  m ()
+fetchNextBlock = do
   env <- ask
 
-  -- Wait if intersection is not found
+  -- Wait until intersection is found
   liftIO $
-    atomically $ do
-      intersection <- readTVar env.intersectionTVar
-      guard $ isJust intersection
+    atomically $
+      readTVar env.intersectionTVar >>= guard . isJust
 
   jsonMsg <- sendAndReceive $ Aeson.encode $ mkRequestNextRequest 0
   let msg = Aeson.eitherDecode @OgmiosRequestNextResponse jsonMsg
   case _result <$> msg of
     Left e ->
       logErrorNS
-        "receiveBlocksLoop"
+        "fetchNextBlock"
         $ Text.pack $ "Error decoding RequestNext response: " <> e
     Right (RollBackward _point _tip) ->
-      logWarnNS "receiveBlocksLoop" "Received RollBackward response"
+      logWarnNS "fetchNextBlock" "Received RollBackward response"
     Right (RollForward OtherBlock _tip) ->
       logWarnNS
-        "receiveBlocksLoop"
+        "fetchNextBlock"
         "Received non-Alonzo block in the RollForward response"
     Right (RollForward (MkAlonzoBlock block) _tip) -> do
       liftIO $ atomically $ writeTBQueue env.queue block
-      logInfoNS "receiveBlocksLoop" $
+      logInfoNS "fetchNextBlock" $
         Text.pack $
           "Fetched block: "
             <> show (slot $ header block, headerHash block)
@@ -268,7 +293,10 @@ newtype BlockProcessorApp a = BlockProcessorApp
   }
   deriving newtype (Functor, Applicative, Monad, MonadIO, MonadLogger, MonadReader BlockProcessorEnv)
 
-processLoop :: BlockProcessorApp ()
+-- | Indefinite loop, takes blocks from queue, extracts data and saves in db.
+processLoop ::
+  (MonadIO m, MonadReader BlockProcessorEnv m, MonadLogger m) =>
+  m ()
 processLoop = do
   logInfoNS "processLoop" "Starting..."
   env <- ask
@@ -278,12 +306,19 @@ processLoop = do
     saveDatumsFromAlonzoBlock block
     updateLastBlock env.dbConn (BlockInfo block.header.slot block.headerHash)
 
-getBlock :: BlockProcessorApp AlonzoBlock
+-- | Pop block for queue, blocking if no block in queue.
+getBlock ::
+  (MonadIO m, MonadReader BlockProcessorEnv m) =>
+  m AlonzoBlock
 getBlock = do
   env <- ask
   liftIO $ atomically $ readTBQueue env.queue
 
-saveDatumsFromAlonzoBlock :: AlonzoBlock -> BlockProcessorApp ()
+-- | Extract and save datums from `AlonzoBlock`.
+saveDatumsFromAlonzoBlock ::
+  (MonadIO m, MonadReader BlockProcessorEnv m, MonadLogger m) =>
+  AlonzoBlock ->
+  m ()
 saveDatumsFromAlonzoBlock block = do
   env <- ask
   datumFilter <- liftIO $ readMVar env.datumFilterMVar
@@ -305,11 +340,9 @@ saveDatumsFromAlonzoBlock block = do
   let datums = Map.toList requestedDatumsWithDecodedValues
   unless (null datums) $ saveDatums env.dbConn datums
 
+-- | Change block processor's datum filer. Safe to call from user facing API.
 changeDatumFilter ::
-  ( MonadIO m
-  , MonadReader r m
-  , Has BlockProcessorEnv r
-  ) =>
+  (MonadIO m, MonadReader r m, Has BlockProcessorEnv r) =>
   DatumFilter ->
   m ()
 changeDatumFilter datumFilter = do
