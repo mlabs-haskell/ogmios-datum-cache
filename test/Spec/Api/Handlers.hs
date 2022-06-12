54f765e6d45e8360a7ef85786a66be3799f1ba3c
t4ccer <t4ccer@gmail.com>
2022-06-12 07:42:26 -0600

    Remove invalid tests

--- test/Spec/Api/Handlers.hs
+++ test/Spec/Api/Handlers.hs
@@ -2,22 +2,13 @@
 
 module Spec.Api.Handlers (spec) where
 
-import Data.ByteString (ByteString)
-import Data.ByteString.Base64 (encode)
-import Network.HTTP.Types.Method (methodPost)
-import Network.Wai.Test (SResponse)
 import Servant.Server (Application)
 import Test.Hspec (Spec, describe, it, pendingWith)
 import Test.Hspec.Wai (
-  ResponseMatcher (matchStatus),
-  WaiSession,
   get,
-  post,
-  request,
   shouldRespondWith,
   with,
  )
-import Test.Hspec.Wai.JSON (json)
 
 spec :: Either String Application -> Spec
 spec (Left err) =
@@ -27,19 +18,3 @@ spec (Right app) = do
     describe "Protect Privileged API is configured" $
       with (return app) $ do
         it "/healthcheck - 200" $ get "/healthcheck" `shouldRespondWith` 200
-        it "/control/cancel_fetch_blocks - 401 Unauthorized" $ do
-          post "/control/cancel_fetch_blocks" ""
-            `shouldRespondWith` 401
-        it "/control/cancel_fetch_blocks with wrong auth - 403 Forbidden" $ do
-          postWithAuth "wrong:wrong" "/control/cancel_fetch_blocks"
-            `shouldRespondWith` 403
-        it "/control/cancel_fetch_blocks with auth - 422 Unprocessable Entity" $ do
-          postWithAuth "usr:pwd" "/control/cancel_fetch_blocks"
-            `shouldRespondWith` [json|{"error": "No block fetcher running"}|]
-              { matchStatus = 422
-              }
-
-postWithAuth :: ByteString -> ByteString -> WaiSession st SResponse
-postWithAuth auth path = request methodPost path headers ""
-  where
-    headers = [("Authorization", "Basic " <> encode auth)]
