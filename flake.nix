3ef9601af25260e22d14bef5a13e6be67d6a5b5c
Rory Tyler Hayford <rory.hayford@protonmail.com>
2022-07-21 10:47:54 +0700

    Add `hydraJobs` with correct system

--- flake.nix
+++ flake.nix
@@ -22,12 +22,8 @@
 
   outputs = { self, nixpkgs, unstable_nixpkgs, ... }:
     let
-      supportedSystems = [
-        "x86_64-linux"
-        "x86_64-darwin"
-        "aarch64-linux"
-        "aarch64-darwin"
-      ];
+      supportedSystems =
+        [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
       perSystem = nixpkgs.lib.genAttrs supportedSystems;
       nixpkgsFor = system: import nixpkgs { inherit system; };
       unstableNixpkgsFor = system: import unstable_nixpkgs { inherit system; };
@@ -97,5 +93,7 @@
             touch $out
           '';
         });
+
+      hydraJobs.x86_64-linux = self.checks.x86_64-linux;
     };
 }
