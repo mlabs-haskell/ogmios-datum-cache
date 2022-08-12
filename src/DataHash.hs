880a69a03fbfd06a4990ba8873f06907d4cd16a7
Luis Alberto Díaz Díaz <73986926+Luis-omega@users.noreply.github.com>
2022-08-12 09:30:31 -0500

    backport change for DataHash

--- src/DataHash.hs
+++ src/DataHash.hs
@@ -1,13 +1,20 @@
 module DataHash (DataHash (..)) where
 
-import Data.Aeson (FromJSON, FromJSONKey, ToJSON (toJSON))
+import Data.Aeson (
+  FromJSON,
+  FromJSONKey,
+  ToJSON (toEncoding, toJSON),
+  ToJSONKey,
+  defaultOptions,
+  genericToEncoding,
+ )
 import Data.Text qualified as Text
 import Servant.API.Generic (Generic)
 
 newtype DataHash = DataHash {unDataHash :: Text.Text}
   deriving stock (Generic, Show, Eq)
-  deriving newtype (Ord, FromJSONKey)
-  deriving anyclass (FromJSON)
+  deriving newtype (Ord, FromJSONKey, ToJSONKey, FromJSON)
 
 instance ToJSON DataHash where
   toJSON = toJSON . unDataHash
+  toEncoding = genericToEncoding defaultOptions
